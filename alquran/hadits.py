28bfb0e8b4def5d247afa9bc3ffdb59856f420e1
chandraacw <anggiangga443@gmail.com>
2025-02-20 04:21:48 +0000

    Reconstruct the code and fix major bugs

--- alquran/hadits.py
+++ alquran/hadits.py
@@ -2,29 +2,34 @@ import requests
 import re
 from alquran.config import API_HADITH_URL, RANGE_PATTERN
 
+
 def get_hadits_list():
     """Mengambil daftar buku hadits dari API"""
     response = requests.get(f"{API_HADITH_URL}/books")
     return response.json()
+
 
 def get_hadits(id_hadits, nomor_hadits):
     """Mengambil riwayat hadits sesuai dengan id nya (tirmidzi, muslim, dsb)"""
     response = requests.get(f"{API_HADITH_URL}/books/{id_hadits}/{nomor_hadits}")
     return response.json()
 
+
 def get_hadits_range(id_hadits, range_nomor):
     """Mengambil riwayat hadits sesuai dengan id dan berdasarkan range nya"""
     response = requests.get(f"{API_HADITH_URL}/books/{id_hadits}?range={range_nomor}")
     return response.json()
 
+
 def daftar_hadith():
     """Menampilkan daftar riwayat-riwayat hadits"""
     hadits = get_hadits_list()
     data = hadits["data"]
-    if hadits["code"] != 200 or hadits["error"] == True:
+    if hadits["code"] != 200 or hadits["error"] is True:
         print(hadits["message"])
         return False
     return data
+
 
 def lihat_hadith(id_hadits, nomor_hadits, range=None):
     """Memberikan isi dari sebuah hadits berdasarkan spesifikasi"""
@@ -35,10 +40,10 @@ def lihat_hadith(id_hadits, nomor_hadits, range=None):
             return False
 
         hadits = get_hadits_range(id_hadits, range[0])
-        if hadits["code"] == 400 and hadits["error"] == True:
+        if hadits["code"] == 400 and hadits["error"] is True:
             print("Tidak bisa >300 range (Masalah performa)")
             return False
-        if hadits["code"] == 404 and hadits["error"] == True:
+        if hadits["code"] == 404 and hadits["error"] is True:
             print(hadits["message"])
             return False
         data = hadits["data"]
@@ -48,7 +53,7 @@ def lihat_hadith(id_hadits, nomor_hadits, range=None):
             print("Nomor hadits tidak boleh kosong kecuali range diberikan")
             return False
         hadits = get_hadits(id_hadits, nomor_hadits)
-        if hadits["code"] == 404 and hadits["error"] == True:
+        if hadits["code"] == 404 and hadits["error"] is True:
             print(hadits["message"])
             return False
         data = hadits["data"]
